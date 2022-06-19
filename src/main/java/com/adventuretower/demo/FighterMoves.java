804ef307fcf6728512aa9d83782a67322600ef78
CamS-World <cstephen13@mail.bw.edu>
2022-06-19 00:23:48 -0400

    Fighter can damage monster hp

--- src/main/java/com/adventuretower/demo/FighterMoves.java
+++ src/main/java/com/adventuretower/demo/FighterMoves.java
@@ -4,8 +4,13 @@ import java.util.Random;
 
 public abstract class FighterMoves {
 
-    int slashAttack(Fighter fighter){
-        fighter.attack();
+    int slashAttack(Fighter fighter, Fiend fiend){
+        if (fighter.attack(fiend) == false){
+            System.out.println("Attack missed!");
+            return fiend.getMonsterHp();
+        }
+//        fighter.attack(fiend);
+
 
         Random ran = new Random();
         int d8 = ran.nextInt(8);
@@ -15,8 +20,11 @@ public abstract class FighterMoves {
         System.out.println(d8);
         System.out.println(fighter.getStrength());
         int damage = d8 + fighter.getStrength();
+        System.out.println("Damage amount: " + damage);
 
-        return damage;
+        fiend.setMonsterHp(fiend.getMonsterHp() - damage);
+        System.out.println("monster's new health: ");
+        return fiend.getMonsterHp();
     };
 
     void tornadoBlade(){
--- src/main/java/com/adventuretower/demo/Test.java
+++ src/main/java/com/adventuretower/demo/Test.java
@@ -3,8 +3,8 @@ package com.adventuretower.demo;
 public class Test {
     public static void main(String[] args) {
         Fighter tony = new Fighter("Tony",50,5,1,2,15,3,1);
+        Fiend jabuzal = new Fiend("Ozy",35,14,150,"Fire","Poison");
 
-
-        System.out.println(tony.slashAttack(tony));
+        System.out.println(tony.slashAttack(tony,jabuzal));
     }
 }
